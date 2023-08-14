6d2f7b371dd5910d4f91b85a37d7bfca4d80c221
Stelly <stellyita@gmail.com>
2023-08-14 22:33:04 +0200

    fix: clean mineSweeper controller

--- src/controllers/mineSweeper.js
+++ src/controllers/mineSweeper.js
@@ -1,50 +1,28 @@
 export function getMatrix(diff) {
 	let matrix = [];
 	let row = [];
-	let w = 10;
-	let h = 10;
+	let w;
+	let h;
 	if (diff == "Easy") {
-		while (w > 0) {
-			row.push(0)
-			w--;
-		}
-		while (h > 0) {
-			matrix.push(row)
-			h--;
-		}
+		w = 10;
+		h = w;
 	} else if (diff == "Medium") {
-		w = 17
-		while (w > 0) {
-			row.push(0)
-			w--;
-		}
-		h = 17
-		while (h > 0) {
-			matrix.push(row)
-			h--;
-		}
+		w = 17;
+		h = w;
 	} else if (diff == "Hard") {
-		w = 24
-		while (w > 0) {
-			row.push(0)
-			w--;
-		}
-		h = 24
-		while (h > 0) {
-			matrix.push(row)
-			h--;
-		}
-	} else {
-		w = 30
-		while (w > 0) {
-			row.push(0)
-			w--;
-		}
-		h = 30
-		while (h > 0) {
-			matrix.push(row)
-			h--;
-		}
+		w = 24;
+		h = w;
+	} else if (diff == "Expert") {
+		w = 30;
+		h = w;
+	}
+	while (w > 0) {
+		row.push(0)
+		w--
+	}
+	while (h > 0) {
+		matrix.push(row)
+		h--
 	}
 	return matrix
 }
